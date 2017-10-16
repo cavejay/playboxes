3b4e8774cd963b23f534dc19befad57632f1b68f
Michael <michael@sablecliff.com>
2017-10-17 06:44:24 +1100

    addeed some more options

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const app = new Cottage();
 // app.get('/game/', async () => 'Hello world!');
 // app.listen(8080);
 
-console.log(Boxes)
-
 var Game = new Boxes(4,4,['player1', 'player2']);
 
-Game.showMap();
+console.log(Game.showMap())
 
