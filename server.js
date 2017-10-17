60c4af2bf6536c5abc8dab26949e1db0d740290a
Michael <michael@sablecliff.com>
2017-10-18 07:11:23 +1100

    have disabled io integration for now. Seems like it might be more involved to get websockets working with cottage/koav2

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const Boxes = require("./lib/boxes").default;
-const IO = require("./lib/io").default;
+// const IO = require("./lib/io").default;
 const Cottage = require("cottage");
 const http = require("http");
 
 const app = new Cottage();
 
-app.post("/new/", async () => "This would create a new game!");
+app.get("/", async () => "This would create a new game!");
 
-var server = http.createServer(app.callback());
+// var server = http.createServer(app.callback());
 
-var io = new IO(server);
+// var io = new IO(server);
 
-server.listen(8080);
+app.listen(9090);
+// server.listen(9090);
+console.log("Server Listening on localhost:9090");
 
 var Game = new Boxes(4, 4, ["player1", "player2"]);
 
