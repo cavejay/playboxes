555d626e8ad4f9377e8caa799fedbd385ad2eeef
Michael <michael@sablecliff.com>
2017-10-20 07:17:30 +1100

    Feat: Websocket to client communication setup

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
-const Boxes = require("./lib/boxes").default;
-// const IO = require("./lib/io").default;
+const logger = require("console-log-level");
+
+// Webstack
+const IO = require("koa-socket-2");
 const Cottage = require("cottage");
-const http = require("http");
+const koa = require("koa");
 
-const app = new Cottage();
+// Game imports
+const Boxes = require("./lib/boxes");
 
-app.get("/", async () => "This would create a new game!");
+// inits
+const log = new logger({ level: "info" });
+const app = new koa();
+const io = new IO();
 
-// var server = http.createServer(app.callback());
+// Webserver middleware init
+app.use(async function logging(ctx, next) {
+  log.info(`Connection from ${ctx.ip} for ${ctx.method} - ${ctx.href}`);
+  await next();
+});
+app.use(require("koa-static")("./web"));
 
-// var io = new IO(server);
+// websocket middleware init
+io.attach(app);
+io.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`WS ${ms}ms`);
+});
 
+// websocket setup
+io.on("connection", (ctx, data) => {
+  console.log("a user connected");
+  ctx.socket.emit("chat message", "Welcome to chat!");
+  ctx.socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  ctx.socket.on("chat message", function(msg) {
+    io.broadcast("chat message", msg);
+  });
+});
+
+// nice routes for webserver using cottage.
+const routes = new Cottage();
+routes.get("/api", async () => "This would create a new game!");
+app.use(routes.callback());
+
+// start server
 app.listen(9090);
-// server.listen(9090);
-console.log("Server Listening on localhost:9090");
+log.info("Listening on localhost:9090");
 
+// Game stuff to check if it all works
 var Game = new Boxes(4, 4, ["player1", "player2"]);
 
 console.log(Game.showInfo());
