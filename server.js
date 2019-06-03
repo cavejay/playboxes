395e80d3ef87e642f68c568480dadb66f8809c5b
Michael <michael@sablecliff.com>
2019-06-03 22:42:41 +1000

    post-merge cleanup

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const log = require("./lib/logFactory").newLog("MAIN");
+// const log = require("./lib/logFactory").newLog("MAIN");
 
 // Webstack
-const koa = require("koa"); // Base server
-const mount = require("koa-mount");
-const api = require("./lib/api").callback();
-const IO = require("./lib/io");
-const static = require("koa-static")("./web");
+// const koa = require("koa"); // Base server
+// const mount = require("koa-mount");
+// const api = require("./lib/api").callback();
+// const IO = require("./lib/io");
+// const static = require("koa-static")("./web");
 
 // inits
 // const app = new koa();
@@ -46,18 +46,6 @@ app.use(express.static("public"));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/dom.html");
-});
-
-app.get("/paper", function(req, res) {
-  res.sendFile(__dirname + "/views/paper.html");
-});
-
-app.get("/pixi", function(req, res) {
-  res.sendFile(__dirname + "/views/pixi.html");
-});
-
-app.get("/two", function(req, res) {
-  res.sendFile(__dirname + "/views/two.html");
 });
 
 app.get("/dom", function(req, res) {
