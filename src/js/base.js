7a3fe6b5b045f5f6058971fa51addd7327466c5e
cavejay <michael@sablecliff.com>
2019-06-18 01:48:20 +1000

    commiting for posterity

---
+++ src/js/base.js
@@ -0,0 +1,128 @@
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Corner(x, y) {
+  this.vline = 0;
+  this.hline = 0;
+  this.value = 0;
+  this.x = x;
+  this.y = y;
+}
+
+Corner.prototype.setVline = function(vLine) {
+  this.vline = vLine || this.vline;
+};
+
+Corner.prototype.setHline = function(hLine) {
+  this.hline = hLine || this.hline;
+};
+
+Corner.prototype.setPos = function(x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Corner.prototype.getPos = function() {
+  return { x: this.x, y: this.y };
+};
+
+function Boxes(len, wid) {
+  this.len = len;
+  this.wid = wid;
+  this.boardArray = [];
+}
+
+Boxes.prototype.initialiseBoard = function initialiseBoard(len, wid) {
+  this.len = len;
+  this.wid = wid;
+
+  for (var i = 0; i <= len; i++) {
+    let row = [];
+    for (var j = 0; j <= wid; j++) {
+      let p = new Corner(j, i);
+      if (j == wid) {
+        p.setHline(-1);
+      }
+      if (i == len) {
+        p.setVline(-1);
+      }
+      row.push(p);
+    }
+    this.boardArray.push(row);
+  }
+
+  return this;
+};
+
+Boxes.prototype.updateLine = function updateLine(start, direction) {
+  console.log("updating line: ", start, direction);
+  if (direction == "h") {
+    this.boardArray[start.y][start.x].setHline(1);
+  } else {
+    this.boardArray[start.y][start.x].setVline(1);
+  }
+
+  // console.log(this.boardArray);
+};
+
+Boxes.prototype.strImport = function strImport() {};
+
+Boxes.prototype.strExport = function strExport() {
+  let r = `${this.wid}${this.len}`;
+  let d = [];
+
+  for (let i = 0; i <= this.len; i++) {
+    d.push(
+      this.boardArray[i]
+        .map((e, i) => {
+          if (e.hline != -1) {
+            return e.hline == 0 ? "?" : e.hline;
+          }
+        })
+        .join("")
+    );
+
+    d.push(
+      this.boardArray[i]
+        .map((e, i) => {
+          let s = "";
+          if (e.vline != -1) {
+            s += e.vline == 0 ? "?" : e.vline;
+            if (e.hline != -1) {
+              s += e.value == 0 ? "!" : e.value;
+            }
+          }
+          // s += e.vline != -1 || e.hline != -1 ? "" : "!";
+          return s;
+        })
+        .join("")
+    );
+  }
+  console.log(d);
+
+  let output = r + d.slice(0, -1).join("|");
+
+  console.log(output);
+  return output;
+};
+
+Boxes.prototype.validateBoard = function validateBoard() {};
+
+var Box1 = new Boxes().initialiseBoard(9, 9);
+
+function lineClick(from, to) {
+  console.log(from + " " + to);
+  let o = "horizontal";
+  if (from.split("-")[0] != to.split("-")[0]) {
+    o = "vertical";
+  }
+
+  let p = new Point(from.split("-")[1] / 1, from.split("-")[0] / 1);
+  Box1.updateLine(p, o[0]);
+
+  drawGame(Box1.strExport());
+}
+
+drawGame(Box1.strExport());
---
+++ src/js/render.dom.js
@@ -0,0 +1,231 @@
+console.log("renderer: DOM ");
+
+let element = document.getElementById("gamespace");
+
+function newDot(id) {
+  return (
+    '<span class="dot" id="' +
+    id +
+    '" onclick="dotClick(' +
+    "'" +
+    id +
+    "'" +
+    ')">•</span>'
+  );
+}
+function vline(from, to, visible) {
+  return (
+    `<span class="vline ${visible ? "" : "invisibleLine"}" id="` +
+    from +
+    "_" +
+    to +
+    '" onclick="lineClick(' +
+    "'" +
+    from +
+    "','" +
+    to +
+    "'" +
+    ')">|</span>'
+  );
+}
+function hline(from, to, visible) {
+  return (
+    `<span class="hline ${visible ? "" : "invisibleLine"}" id="` +
+    from +
+    "_" +
+    to +
+    '" onclick="lineClick(' +
+    "'" +
+    from +
+    "','" +
+    to +
+    "'" +
+    ')">—</span>'
+  );
+}
+function square(id, val) {
+  return (
+    '<span class="box" id="' +
+    id +
+    '" onclick="fillBox(' +
+    "'" +
+    id +
+    "'" +
+    `)">${val && val != "!" ? val[0] : " "}</span>`
+  );
+}
+let vlineF = '<span class="vline">|</span>';
+let hlineF = '<span class="hline">—</span>';
+
+function dotClick(id) {
+  console.log(id);
+}
+
+function fillBox(id) {
+  return;
+  let b = document.getElementById(id);
+  b.setAttribute("class", "boxFill");
+  console.log("filled box " + id);
+}
+
+function makeGrid(width, height) {
+  width++;
+  height++;
+  let ret = "";
+  for (let i = 0; i < height * 2; i++) {
+    if (i % 2 == 0) {
+      for (let j = 0; j < width; j++) {
+        let dcoord = Math.floor(i / 2) + "-" + j;
+        ret += newDot(dcoord);
+        if (j < width - 1) {
+          ret += hline(dcoord, Math.floor(i / 2) + "-" + (j + 1));
+        }
+      }
+    } else {
+      if (i > height * 2 - 2) break;
+      for (let j = 0; j < width; j++) {
+        let dcoord = Math.floor(i / 2) + "-" + j;
+        ret += vline(dcoord, Math.floor(i / 2) + 1 + "-" + j);
+        if (j < width - 1) {
+          ret += square("sqrd" + dcoord);
+        }
+      }
+    }
+    ret += "<br>";
+  }
+  return ret;
+}
+
+// Make the DIV element draggable:
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("gamespace"));
+
+/**
+ * Steps
+ *
+ * Data representation
+ * Update + export data
+ * Create from data representation
+ *
+ */
+
+// '22 ?? ?!?!? ?? ?!?!? ??'
+function simpleEmptyGrid(side) {
+  let r = `${side}${side}`;
+  let d = [];
+  for (let i = 0; i < side * 2 + 1; i++) {
+    d.push(
+      i % 2
+        ? new Array(side * 2 + 1)
+            .fill("?")
+            .map((e, i) => (i % 2 ? "!" : e))
+            .join("")
+        : new Array(side + 1).join("?")
+    );
+  }
+  return r + d.join("|");
+}
+
+let gridstr = simpleEmptyGrid(9);
+console.log(gridstr);
+
+// ?? ?!?!? ?? ?!?!? ??
+function gridFromString(str) {
+  let width = str[0];
+  let height = str[1];
+  let data = str.slice(2).split("|");
+
+  let ret = "";
+  for (let i = 0; i <= height * 2 + 1; i++) {
+    if (i % 2 == 0) {
+      // horizontal Line render (no boxes)
+      for (let j = 0; j <= width; j++) {
+        let dcoord = Math.floor(i / 2) + "-" + j;
+        ret += newDot(dcoord);
+        if (j <= width - 1) {
+          ret += hline(
+            dcoord,
+            Math.floor(i / 2) + "-" + (j + 1),
+            data[i][j] != "?"
+          );
+        }
+      }
+    } else {
+      // vertical + bo line render
+      if (i > height * 2 - 1) break;
+      for (let j = 0; j <= width * 2 + 1; j++) {
+        let dcoord = Math.floor(i / 2) + "-" + j / 2;
+        if (j % 2 == 0) {
+          ret += vline(
+            dcoord,
+            Math.floor(i / 2) + 1 + "-" + j / 2,
+            data[i][j] != "?"
+          );
+        } else {
+          ret += square("sqr" + dcoord, data[i][j]);
+        }
+      }
+    }
+    ret += "<br>";
+  }
+  return ret;
+}
+
+let g2 = "22??|?!?!?|??|?!?!?|??";
+console.log(g2);
+
+function drawGame(gridstr) {
+  let element = document.getElementById("gamespace");
+  element.innerHTML = gridFromString(gridstr);
+}
+
+// drawGame(g2);
+
+// function lineClick(from, to) {
+//   console.log(from + " " + to);
+//   let o = "horizontal";
+//   if (from.split("-")[0] != to.split("-")[0]) {
+//     o = "vertical";
+//   }
+//   console.log(o);
+//   let line = document.getElementById(from + "_" + to);
+//   line.setAttribute("class", "hline");
+// }
